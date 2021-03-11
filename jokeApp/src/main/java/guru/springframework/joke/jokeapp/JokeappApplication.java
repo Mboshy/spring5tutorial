13131c96de49cfd408cedd707074ea76016bea5a
Mboshy <bosakmich@gmail.com>
2021-03-11 17:12:32 +0100

    files fix

--- jokeApp/src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
+++ jokeApp/src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
@@ -1,13 +1,13 @@
-package guru.springframework.joke.demo;
+package guru.springframework.joke.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class JokeappApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(JokeappApplication.class, args);
     }
 
 }
--- jokeApp/src/test/java/guru/springframework/joke/jokeapp/JokeappApplicationTests.java
+++ jokeApp/src/test/java/guru/springframework/joke/jokeapp/JokeappApplicationTests.java
@@ -1,10 +1,10 @@
-package guru.springframework.joke.demo;
+package guru.springframework.joke.jokeapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class JokeappApplicationTests {
 
     @Test
     void contextLoads() {
