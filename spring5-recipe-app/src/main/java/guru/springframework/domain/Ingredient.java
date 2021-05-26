a933560259f94ab2e675fb4872341fd15d6451ce
MBoshy <bosakmich@gmail.com>
2021-05-26 13:29:19 +0200

    lombok added

--- spring5-recipe-app/src/main/java/guru/springframework/domain/Ingredient.java
+++ spring5-recipe-app/src/main/java/guru/springframework/domain/Ingredient.java
@@ -1,9 +1,13 @@
 package guru.springframework.domain;
 
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
     @Id
@@ -32,46 +36,6 @@ public class Ingredient {
         this.description = description;
         this.amount = amount;
         this.recipe = recipe;
-        this.uom = uom;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUnitOfMeasure() {
-        return uom;
-    }
-
-    public void setUnitOfMeasure(UnitOfMeasure uom) {
         this.uom = uom;
     }
 }
